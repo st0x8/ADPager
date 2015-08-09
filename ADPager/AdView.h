01f1ef96e93bf8d105bc8e43a57412d684b3ed8c
st0x8 <ste0x08@gmai.com>
2015-08-09 15:24:46 +0800

    Delete useless file

---
+++ ADPager/AdView.h
@@ -0,0 +1,13 @@
+//
+//  AdView.h
+//  ADPager
+//
+//  Created by lin on 15/8/9.
+//  Copyright (c) 2015年 Roy Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AdView : UIView <UIScrollViewDelegate>
+
+@end
---
+++ ADPager/AdView.m
@@ -0,0 +1,17 @@
+//
+//  AdView.m
+//  ADPager
+//
+//  Created by lin on 15/8/9.
+//  Copyright (c) 2015年 Roy Lin. All rights reserved.
+//
+
+#import "AdView.h"
+/*
+ * 
+ */
+@implementation AdView
+
+
+
+@end
